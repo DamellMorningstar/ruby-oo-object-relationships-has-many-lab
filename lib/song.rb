253ea65398f48e96813d05289a7db1e9502f6dd0
Da-Mell <Damellfaines8@gmail.com>
2021-02-05 10:15:37 -0500

    commit tree cbe29d33a95b8ffab45fff3c86ed9dd4bc9de809

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+
+# class Song 
+    
+#     @@all = []
+#     attr_accessor :title, :artist
+
+#     def initialize(artist_instance,title)
+#         @artist_instance = artist_name
+#         @title = title
+
+#         @@all << self
+#     end
+
+#     def self.all
+#         @@all
+#     end
+# end
